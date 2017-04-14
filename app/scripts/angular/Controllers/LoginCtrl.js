ade9b6859285f21f4669aac1ea1fa7e21531c689
william.w.jing <william.w.jing@pwc.com>
2017-04-14 11:08:49 +0800

    add error message and validation

--- app/scripts/angular/Controllers/LoginCtrl.js
+++ app/scripts/angular/Controllers/LoginCtrl.js
@@ -1,20 +1,24 @@
 app.controller('LoginCtrl', function ($scope, $http) {
 
-    $scope.login = function (loginInfo) {
-        console.log(loginInfo);
-        var url = 'http://localhost:8181/user/verification';
-        loginInfo.password = md5(loginInfo.password);
-        $http.post(url, loginInfo).then(function (data) {
-            console.log(data);
-            if (data.data.msg === "success") {
-                //TODO
-                alert("Login Succeed");
-                $scope.init();
-            } else {
-                alert("Login Failed");
-                $scope.init();
-            }
-        })
+    $scope.login = function (isValid,loginInfo) {
+        $scope.submitted = true;
+        if(isValid){
+            console.log(loginInfo);
+            var url = 'http://localhost:8181/user/verification';
+            loginInfo.password = md5(loginInfo.password);
+            $http.post(url, loginInfo).then(function (data) {
+                console.log(data);
+                if (data.data.msg === "success") {
+                    //TODO
+                    alert("Login Succeed");
+                    $scope.init();
+                } else {
+                    alert("Login Failed");
+                    $scope.init();
+                }
+            })
+        }
+        
     }
 
     $scope.init= function(){
--- app/scripts/angular/Controllers/SignupCtrl.js
+++ app/scripts/angular/Controllers/SignupCtrl.js
@@ -1,39 +1,43 @@
 app.controller('SignupCtrl', function ($scope, $http, $timeout) {
 
-    $scope.signup = function (signupInfo) {
-        console.log(signupInfo);
-        signupInfo.password = md5(signupInfo.password);
+    $scope.signup = function (isValid, signupInfo) {
+        $scope.submitted = true;
+        if(isValid){
+            console.log(signupInfo);
+            signupInfo.password = md5(signupInfo.password);
 
-        var url = 'http://localhost:8181/user/saving';
-        $http.post(url, signupInfo).then(function (data) {
-            console.log(data);
-            if (data.data.msg === "success") {
-                //TODO
-                $scope.init();
-                alert("Signup Succeed");
-            } else {
-                $scope.init();
-                alert("Signup Failed");
-            }
-        })
+            var url = 'http://localhost:8181/user/saving';
+            $http.post(url, signupInfo).then(function (data) {
+                console.log(data);
+                if (data.data.msg === "success") {
+                    //TODO
+                    $scope.init();
+                    alert("Signup Succeed");
+                } else {
+                    $scope.init();
+                    alert("Signup Failed");
+                }
+            })
+        }
+        
     }
 
     $scope.init = function () {
         $scope.signupInfo = {};
     }
 
-    $scope.validate = function(){
+    $scope.validate = function () {
         $timeout.cancel($scope.time);
 
-        $scope.time = $timeout(function(){
+        $scope.time = $timeout(function () {
             var url = 'http://localhost:8181/user/';
-            $http.get(url+$scope.signupInfo.account+'/verification').then(function (data) {
-                if(data.data.msg === "valid"){
+            $http.get(url + $scope.signupInfo.account + '/verification').then(function (data) {
+                if (data.data.msg === "valid") {
                     console.log(data.data.msg);
-                }else{
+                } else {
                     console.log(data.data.msg);
                 }
             });
-        },3000);
+        }, 3000);
     }
 });
