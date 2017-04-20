1d2e7b9dd539b6880d395f6455a78b316d2a1eec
william.w.jing <william.w.jing@pwc.com>
2017-04-20 11:05:48 +0800

    logout function

--- app/scripts/angular/Controllers/WelcomeCtrl.js
+++ app/scripts/angular/Controllers/WelcomeCtrl.js
@@ -1,7 +1,17 @@
-app.controller('WelcomeCtrl',function($scope, $state, $stateParams, $cookies){
-    if($cookies.getObject("user") !== undefined){
-        $scope.username = $cookies.getObject("user").data.name;
-    }else{
-         $state.go('login');
+app.controller('WelcomeCtrl',function($scope, $rootScope, $state, $stateParams, $cookies){
+    $scope.init = function(){
+        $rootScope.username = null;
+        
+        if($cookies.getObject("user") !== undefined){
+            $rootScope.username = $cookies.getObject("user").data.name;
+        }else{
+            $state.go('login');
+        }
+    }
+
+    $scope.logout = function(){
+        $cookies.remove("user");
+        $rootScope.username = null;
+        $scope.init();
     }
 });
